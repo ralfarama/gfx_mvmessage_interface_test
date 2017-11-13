4889e343c498b7752d0c740c077c89432bbb39fc
Ralph Santos <ralfarama@yahoo.com>
2017-11-13 04:41:30 -0800

    first cut port Adafruit demo

--- bw_movingsign.h
+++ bw_movingsign.h
@@ -15,11 +15,24 @@
 class bw_movingsign : public Adafruit_GFX {
  public:
 
-  bw_movingsign(int16_t w, int16_t h);
+  bw_movingsign(void);
   
+  void movingsign_bootstrap(void);
   void drawPixel(int16_t x, int16_t y, uint16_t color);
-
+  void clear(void);
+  void set_buffer_column(int x, byte c);
+  void set_column_strobe_interval(int t);
+  void set_autobufferupdate(byte t);
+  void writeDisplay(void);
+  
  private:
+  byte drawbuffer[];
+  byte displaybuffer[];
+  byte autobufferupdate;
+  int column_strobe_interval;
+  void set_matrix_col_latch(int latch_num, int latch_bit, int latch_set);
+  void set_matrix_bank_register(int bank,int reg_value);
+  void enable_col(int colnum);
 };
 
 #endif // - ifndef bw_movingign_h
